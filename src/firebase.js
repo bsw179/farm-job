bf638ee0eed1b89bdaf5b8b434dd941cf48221e4
bsw179 <bsw179@gmail.com>
2025-03-28 01:02:11 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
-
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBYzAhsCVeIH-Fh1HBTrjpl-r8pkS-h1TY",
   authDomain: "farm-job.firebaseapp.com",
   projectId: "farm-job",
-  storageBucket: "farm-job.firebasestorage.app",
+  storageBucket: "farm-job.appspot.com",
   messagingSenderId: "352177686150",
   appId: "1:352177686150:web:d9e81d0a95ca14378c7307"
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+const db = getFirestore(app);
+
+export default db;
