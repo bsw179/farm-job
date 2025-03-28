c394b5b2c9e5a136876ba34b4d8514e9e70f8318
bsw179 <bsw179@gmail.com>
2025-03-28 01:02:47 -0500

    Update Fields.jsx

--- src/pages/Fields.jsx
+++ src/pages/Fields.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { collection, addDoc, getDocs, onSnapshot } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot } from 'firebase/firestore';
 import db from '../firebase';
 
 export default function Fields() {
