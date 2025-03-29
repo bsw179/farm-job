1bd2b476d011f15d22d17c5644050369251dc8ab
bsw179 <bsw179@gmail.com>
2025-03-29 15:16:41 -0500

    Update AppLayout.jsx

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import TopBar from './components/TopBar';
-import { CropYearProvider } from './context/CropYearContext';
 import Fields from './pages/Fields';
 import FieldDetail from './pages/FieldDetail';
 import BoundaryUpload from './pages/BoundaryUpload';
+import { CropYearProvider } from './context/CropYearContext';
 
 export default function AppLayout() {
   return (
@@ -15,14 +15,15 @@ export default function AppLayout() {
         <Sidebar />
         <main className="flex-1 bg-gradient-to-br from-gray-50 to-white overflow-y-auto">
           <TopBar />
-          <Routes>
-            <Route path="/" element={<Fields />} />
-            <Route path="/fields/:fieldId" element={<FieldDetail />} />
-            <Route path="/boundary-upload" element={<BoundaryUpload />} />
-          </Routes>
+          <div className="p-6">
+            <Routes>
+              <Route path="/" element={<Fields />} />
+              <Route path="/fields/:fieldId" element={<FieldDetail />} />
+              <Route path="/boundary-upload" element={<BoundaryUpload />} />
+            </Routes>
+          </div>
         </main>
       </div>
     </CropYearProvider>
   );
 }
-
