85533c7d0b78761de014042991001c434c0d5c58
bsw179 <bsw179@gmail.com>
2025-03-29 12:37:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       buffer: 'buffer',
+      './window': path.resolve(__dirname, 'src/shims/empty.js'),
     },
   },
   optimizeDeps: {
