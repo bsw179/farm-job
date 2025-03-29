5b7a773e331ed70d9fab5a14b98cb16452353891
bsw179 <bsw179@gmail.com>
2025-03-28 20:53:23 -0500

    Update MapViewer.jsx

--- src/pages/MapViewer.jsx
+++ src/pages/MapViewer.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
+export default function MapViewer() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Map Viewer</h2>
+      <MapContainer
+        center={[34.789, -91.687]} // Center on AR/MS Delta
+        zoom={10}
+        scrollWheelZoom={true}
+        style={{ height: '600px', width: '100%', borderRadius: '0.5rem' }}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
