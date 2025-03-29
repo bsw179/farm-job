884eb551a6e737ec1a96e06176997863072c973e
bsw179 <bsw179@gmail.com>
2025-03-29 14:16:28 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 // src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBYzAhsCVeIH-Fh1HBTrjpl-r8pkS-h1TY",
   authDomain: "farm-job.firebaseapp.com",
   projectId: "farm-job",
-  storageBucket: "farm-job.appspot.com",
+  storageBucket: "farm-job.firebasestorage.app",
   messagingSenderId: "352177686150",
   appId: "1:352177686150:web:d9e81d0a95ca14378c7307"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export default db;
+// ✅ Add this line to export your Firestore database
+const db = getFirestore(app);
+export { db };
