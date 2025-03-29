a0ed45f3b0e0544d1d53dca4db48cb98fa9fb081
bsw179 <bsw179@gmail.com>
2025-03-28 19:26:35 -0500

    Create placeholder ImportFields component

---
+++ src/pages/ImportFields.jsx
@@ -0,0 +1,8 @@
+export default function ImportFields() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Import Fields</h2>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
