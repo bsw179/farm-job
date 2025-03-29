cddeaeb43cd3c0b3f73b93a627840fbe09efe9a5
bsw179 <bsw179@gmail.com>
2025-03-29 15:14:49 -0500

    Update AppLayout.jsx

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -6,20 +6,23 @@ import TopBar from './components/TopBar';
 import { CropYearProvider } from './context/CropYearContext';
 import Fields from './pages/Fields';
 import FieldDetail from './pages/FieldDetail';
+import BoundaryUpload from './pages/BoundaryUpload';
 
 export default function AppLayout() {
   return (
     <CropYearProvider>
       <div className="flex min-h-screen font-sans text-gray-800">
         <Sidebar />
-        <main className="flex-1 bg-gradient-to-br from-gray-50 to-white">
+        <main className="flex-1 bg-gradient-to-br from-gray-50 to-white overflow-y-auto">
           <TopBar />
           <Routes>
             <Route path="/" element={<Fields />} />
             <Route path="/fields/:fieldId" element={<FieldDetail />} />
+            <Route path="/boundary-upload" element={<BoundaryUpload />} />
           </Routes>
         </main>
       </div>
     </CropYearProvider>
   );
 }
+
